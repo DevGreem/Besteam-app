6e518a6e8ddbab93a5ba464e980cd59a5ea814a2
Ian Pichardo <ian.pichardo@hotmail.com>
2026-01-16 12:37:28 -0400

    change: Only import QApplication in main.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow
-from PyQt5.QtCore import * # type: ignore
-from PyQt5.QtGui import * # type: ignore
+from PyQt5.QtWidgets import QApplication
 from dotenv import load_dotenv
 from ui import MainWindow
 
 load_dotenv()
-
 
 if __name__ == "__main__":
     APP: QApplication = QApplication(sys.argv)
